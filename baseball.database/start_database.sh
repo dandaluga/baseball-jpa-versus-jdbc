5d3649b20d6406a3b3acd64435ca830af6f0f78f
Dan DaLuga <dan@daluga.com>
2015-07-07 21:28:38 -0500

    Framework updates

--- baseball.database/start_database.sh
+++ baseball.database/start_database.sh
@@ -1,9 +1,9 @@
 #-------------------------------------------------------------------
 # Harded coded path to HSQLDB location.
 #-------------------------------------------------------------------
-java -cp /Users/dandaluga/.m2/repository/org/hsqldb/hsqldb/2.3.2/hsqldb-2.3.2.jar org.hsqldb.Server -database.0 file:baseball -dbname.0 baseball 
+java -cp /Users/dandaluga/.m2/repository/org/hsqldb/hsqldb/2.3.3/hsqldb-2.3.3.jar org.hsqldb.Server -database.0 file:baseball -dbname.0 baseball 
 
 #-------------------------------------------------------------------
 # Using HSQLDB_HOME environment variable.
 #-------------------------------------------------------------------
-#java -cp $HSQLDB_HOME/hsqldb-2.3.2.jar org.hsqldb.Server -database.0 file:baseball -dbname.0 baseball 
+#java -cp $HSQLDB_HOME/hsqldb-2.3.3.jar org.hsqldb.Server -database.0 file:baseball -dbname.0 baseball 
--- baseball.database/start_database_client.sh
+++ baseball.database/start_database_client.sh
@@ -1,9 +1,9 @@
 #-------------------------------------------------------------------
 # Harded coded path to HSQLDB location.
 #-------------------------------------------------------------------
-java -cp /Users/dandaluga/.m2/repository/org/hsqldb/hsqldb/2.3.2/hsqldb-2.3.2.jar org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://localhost/baseball
+java -cp /Users/dandaluga/.m2/repository/org/hsqldb/hsqldb/2.3.3/hsqldb-2.3.3.jar org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://localhost/baseball
 
 #-------------------------------------------------------------------
 # Using HSQLDB_HOME environment variable.
 #-------------------------------------------------------------------
-#java -jar $HSQLDB_HOME/hsqldb-2.3.2.jar org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://localhost/baseball
+#java -jar $HSQLDB_HOME/hsqldb-2.3.3.jar org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://localhost/baseball
